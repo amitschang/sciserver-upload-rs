8488761ad8c2cafd30ec5000157c884ad429cf6b
Arik Mitschang <arik@jhu.edu>
2025-08-29 14:22:24 -0400

    write error report at end, progress consumes info

--- src/lib.rs
+++ src/lib.rs
@@ -108,40 +108,45 @@ async fn upload_file(client: Client, file_path: String, settings: Arc<Settings>)
 }
 
 struct UploadProgress {
-    total: usize,
-    success: usize,
-    error: usize,
+    n_total: usize,
+    n_successes: usize,
+    n_errors: usize,
     n_retries: usize,
     f_retries: usize,
     bytes: u64,
     timer: Instant,
+    completed: Vec<UploadInfo>,
 }
 
 impl UploadProgress {
-    fn new(total: usize) -> Self {
+    fn new(n_total: usize) -> Self {
         UploadProgress {
-            total,
-            success: 0,
-            error: 0,
+            n_total,
+            n_successes: 0,
+            n_errors: 0,
             n_retries: 0,
             f_retries: 0,
             bytes: 0,
             timer: Instant::now(),
+            completed: Vec::with_capacity(n_total),
         }
     }
 
-    fn update(&mut self, info: &UploadInfo, write_status: bool) {
+    fn update(&mut self, info: UploadInfo, write_status: bool) {
         if info.error.is_some() {
-            self.error += 1;
+            self.n_errors += 1;
         }
         else {
-            self.success += 1;
+            self.n_successes += 1;
             self.bytes += info.bytes;
         }
         if info.retries > 0 {
             self.n_retries += info.retries;
             self.f_retries += 1;
         }
+
+        self.completed.push(info);
+
         if write_status {
             self.write_status_bar();
         }
@@ -153,13 +158,35 @@ impl UploadProgress {
         let mbps = mbs / (elapsed + 1e-6);
 
         format!("Uploaded {}/{} files, {} errors {}|{} retries {:.2} MB in {:.2} seconds ({:.2} MB/s)",
-               self.success, self.total, self.error, self.f_retries, self.n_retries, mbs, elapsed, mbps)
+               self.n_successes, self.n_total, self.n_errors, self.f_retries, self.n_retries, mbs, elapsed, mbps)
     }
 
     fn write_status_bar(&self) {
         let msg = self.status_bar();
         print!("\r{}", msg);
         io::stdout().flush().unwrap();
+    }
+
+    fn write_error_report(&self) {
+        let mut heading_written = false;
+        for info in &self.completed {
+            if let Some(error) = &info.error {
+                if !heading_written {
+                    eprintln!("Error Report:");
+                    heading_written = true;
+                }
+                match error {
+                    ErrorKind::ReadError => eprintln!(
+                        "  Failed to read file: {}", info.path),
+                    ErrorKind::FileExists => eprintln!(
+                        "  File already exists (use --force to overwrite): {}", info.path),
+                    ErrorKind::Unauthorized => eprintln!(
+                        "  Unauthorized (check your token): {}", info.path),
+                    ErrorKind::Other => eprintln!(
+                        "  Failed to upload file after {} retries: {}", info.retries, info.path),
+                }
+            }
+        }
     }
 }
 
@@ -217,22 +244,21 @@ pub async fn upload_many(files: Vec<String>, settings: Arc<Settings>) {
                 // Early stoppage since unath is expected to cause errors in all
                 // other uploads using the same token.
                 if let Some(ErrorKind::Unauthorized) = info.error {
-                    eprintln!("Unauthorized: Check your token.");
+                    eprintln!("\nUnauthorized: Check your token.");
+                    progress.write_error_report();
                     return;
                 }
-                if let Some(ErrorKind::ReadError) = info.error {
-                    eprintln!("Error reading file: {}", info.path);
-                }
                 // TODO: could also stop if the error rate after some point is too high
-                progress.update(&info, true)
+                progress.update(info, true);
             },
-            Err(e) => { eprintln!("Join Error: {:?}", e); }
+            Err(e) => { eprintln!("Unexpected Join Error: {:?}", e); }
         }
         if let Some(file) = files_iter.next() {
             tasks.spawn(upload_file(client.clone(), file, settings.clone()));
         }
     }
     println!();
+    progress.write_error_report();
 }
 
 
@@ -244,14 +270,14 @@ mod tests {
     fn test_progress_bar() {
         let mut progress = UploadProgress::new(10);
         // regular success and error uploads
-        progress.update(&UploadInfo::new("test1.txt".to_string()).with_success(), false);
-        progress.update(&UploadInfo::new("test2.txt".to_string()).with_error(ErrorKind::Other), false);
-        progress.update(&UploadInfo::new("test3.txt".to_string()).with_success(), false);
+        progress.update(UploadInfo::new("test1.txt".to_string()).with_success(), false);
+        progress.update(UploadInfo::new("test2.txt".to_string()).with_error(ErrorKind::Other), false);
+        progress.update(UploadInfo::new("test3.txt".to_string()).with_success(), false);
         // upload with retries
         let mut info = UploadInfo::new("test4.txt".to_string());
         info.incr_retries();
         info.incr_retries();
-        progress.update(&info.with_success(), false);
+        progress.update(info.with_success(), false);
         let status = progress.status_bar();
         // timing is not deterministic, so we just check the beginning prior to
         // time info
